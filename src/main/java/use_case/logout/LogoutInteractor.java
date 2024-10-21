4b17b2c7952a03a17e05797326fb634146b3a0a1
James Lim <63968449+LimJames180@users.noreply.github.com>
2024-10-21 18:47:32 -0400

    Test case working

--- src/main/java/use_case/logout/LogoutInteractor.java
+++ src/main/java/use_case/logout/LogoutInteractor.java
@@ -1,33 +1,29 @@
 package use_case.logout;
 
+import data_access.InMemoryUserDataAccessObject;
+
 /**
- * The Logout Interactor.
+ * The LogoutInteractor class is responsible for handling the logout use case.
  */
 public class LogoutInteractor implements LogoutInputBoundary {
-    private LogoutUserDataAccessInterface userDataAccessObject;
-    private LogoutOutputBoundary logoutPresenter;
+    private final InMemoryUserDataAccessObject userRepository;
+    private final LogoutOutputBoundary presenter;
 
-    public LogoutInteractor(LogoutUserDataAccessInterface userDataAccessInterface,
-                            LogoutOutputBoundary logoutOutputBoundary) {
-        // TODO: save the DAO and Presenter in the instance variables.
-        // Which parameter is the DAO and which is the presenter?
-        this.userDataAccessObject = userDataAccessInterface;
-        this.logoutPresenter = logoutOutputBoundary;
+    public LogoutInteractor(InMemoryUserDataAccessObject userRepository, LogoutOutputBoundary presenter) {
+        this.userRepository = userRepository;
+        this.presenter = presenter;
     }
 
     @Override
-    public void execute(LogoutInputData logoutInputData) {
-        // TODO: implement the logic of the Logout Use Case (depends on the LogoutInputData.java TODO)
-        // * get the username out of the input data,
-        // * set the username to null in the DAO
-        // * instantiate the `LogoutOutputData`, which needs to contain the username.
-        // * tell the presenter to prepare a success view.
-
-        final String username = logoutInputData.getUsername();
-        userDataAccessObject.setCurrentUser(null);
-        final LogoutOutputData logoutOutputData = new LogoutOutputData(username);
-        logoutPresenter.prepareSuccessView(logoutOutputData);
-
+    public void execute(LogoutInputData inputData) {
+        final String username = inputData.getUsername();
+        if (userRepository.getCurrentUsername().equals(username)) {
+            userRepository.setCurrentUsername(null);
+            final LogoutOutputData outputData = new LogoutOutputData(username);
+            presenter.prepareSuccessView(outputData);
+        }
+        else {
+            presenter.prepareFailView("User is not logged in.");
+        }
     }
 }
-
